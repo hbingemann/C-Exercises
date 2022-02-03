e3f9ac83f153f7c015883d86967d76c4ae833a32
hbing1 <hbingemann@mgrhsstudent.org>
2022-02-03 16:17:37 +0000

    finish minscanf exercise

--- chapter7/minscanf.c
+++ chapter7/minscanf.c
@@ -3,73 +3,113 @@
 #include <ctype.h>
 #include <stdlib.h>
 
-void minscanf(char *fmt, ...);
+#define BUFSIZE 100
+
+char charbuf[BUFSIZE];
+int cp = -1;
+
+int minscanf(char *fmt, ...);
+char getch(void);
+void ungetch(char);
+
 
 int main()
 {
-    int d, y;
-    char m[100];
-    if (scanf("%5s/%d/%d", m, &d, &y) == 3)
-        printf("month: %s, day: %d, year: %d\n", m, d, y);
+    int month, day, year;
+    if (minscanf("%d/%d/%d", &month, &day, &year) == 3)
+        printf("month: %d, day: %d, year: %d\n", month, day, year);
     else
         printf("Invalid input\n");
 }
 
 
 /* minscanf: minimal scanf with variable argument list */
-void minscanf(char *fmt, ...)
+int minscanf(char *fmt, ...)
 {
     va_list ap;
     char *p, *sval, c;
-    int ival;
-    double dval;
-    int fwidth;
+    int ival = 0;
+    double dval = 0;
+    int args_eval = 0;
+
+    while ((c = getch()) == ' ' || c == '\n' || c == '\t')  /*skip whitespace*/
+        if (c == EOF)
+            return args_eval;
 
     va_start(ap, fmt);
-    while ((c = getchar()) != ' ' && c != '\n' && c != '\t' && c != EOF); /*skip whitespace*/
-
-    // compare getchar and format string
-    // if they are the same continue
-    // if format string has a %
-        // get next letter and then check for int / double / string
-        // keep getting next char until getchar and format string are equal
-
-    for (p = fmt; *p; p++) {
-        fwidth = 0;
+    for (p = fmt; *p && c && c != EOF; p++, c = getch())
+    {
         if (*p != '%') {
-            putchar(*p);
-            continue;
-        }
-        while ((c = *++p) && c != ' ')
-        {
-            if (isalpha(c)) {
-                switch (c) {
-                    case 'd':
-                        ival = va_arg(ap, int);
-                        fwidth -= printf("%d", ival);
-                        break;
-                    case 'f':
-                        dval = va_arg(ap, double);
-                        fwidth -= printf("%f", dval);
-                        break;
-                    case 's':
-                        for (sval = va_arg(ap, char *); *sval; sval++)
-                        {
-                            putchar(*sval);
-                            fwidth--;
-                        }
-                        break;
-                    default:
-                        putchar(c);
-                        break;
-                }
-                break;  /* always done at letter */
-            } else if (isdigit(c)) {
-                fwidth = fwidth * 10 + (c - '0');
+            if (*p == c)
+                continue;
+            else
+                return args_eval;
+        } else {
+            switch (*++p) {
+                case 'd':
+                    while (isdigit(c))
+                    {
+                        ival = ival * 10 + (c - '0');
+                        c = getch();
+                    }
+                    ungetch(c);
+
+                    *(va_arg(ap, int *)) = ival;
+                    ival = 0;
+                    args_eval++;
+                    break;
+                default:
+                    return args_eval;  /* we only expect a 'd' after '%' */
             }
         }
-        while (fwidth-- > 0)
-            putchar(' ');
     }
+
+    return args_eval;
+
+
+    //     while ((c = *++p) && c != ' ')
+    //     {
+    //         if (isalpha(c)) {
+    //             switch (c) {
+    //                 case 'd':
+    //                     ival = va_arg(ap, int);
+    //                     fwidth -= printf("%d", ival);
+    //                     break;
+    //                 case 'f':
+    //                     dval = va_arg(ap, double);
+    //                     fwidth -= printf("%f", dval);
+    //                     break;
+    //                 case 's':
+    //                     for (sval = va_arg(ap, char *); *sval; sval++)
+    //                     {
+    //                         putchar(*sval);
+    //                         fwidth--;
+    //                     }
+    //                     break;
+    //                 default:
+    //                     putchar(c);
+    //                     break;
+    //             }
+    //             break;  /* always done at letter */
+    //         } else if (isdigit(c)) {
+    //             fwidth = fwidth * 10 + (c - '0');
+    //         }
+    //     }
+    //     while (fwidth-- > 0)
+    //         putchar(' ');
+    // }
     va_end(ap);
+}
+
+char getch(void)
+{
+    return cp < 0 ? getchar() : charbuf[cp--];
+}
+
+void ungetch(char c)
+{
+    if (cp >= BUFSIZE)
+        printf("ungetch: too many characters");
+    else
+        charbuf[++cp] = c;
 }
